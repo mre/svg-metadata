788100538a652adf35441aecd13e2c6bb6f75ca5
Matthias <matthias-endler@gmx.net>
2023-09-08 11:30:10 +0200

    Renamed error::MetadataError to error::Metadata internally

--- src/error.rs
+++ src/error.rs
@@ -7,44 +7,44 @@ use std::num::ParseFloatError;
 #[derive(Debug)]
 /// The error type of the library, which gets
 /// returned on parsing issues.
-pub struct MetadataError {
+pub struct Metadata {
     details: String,
 }
 
-impl MetadataError {
-    pub fn new(msg: &str) -> MetadataError {
-        MetadataError {
+impl Metadata {
+    pub fn new(msg: &str) -> Metadata {
+        Metadata {
             details: msg.to_string(),
         }
     }
 }
 
-impl fmt::Display for MetadataError {
+impl fmt::Display for Metadata {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.details)
     }
 }
 
-impl Error for MetadataError {
+impl Error for Metadata {
     fn description(&self) -> &str {
         &self.details
     }
 }
 
-impl From<ParseFloatError> for MetadataError {
-    fn from(_: ParseFloatError) -> MetadataError {
-        MetadataError::new("Cannot convert string to float")
+impl From<ParseFloatError> for Metadata {
+    fn from(_: ParseFloatError) -> Metadata {
+        Metadata::new("Cannot convert string to float")
     }
 }
 
-impl From<IoError> for MetadataError {
-    fn from(e: IoError) -> MetadataError {
-        MetadataError::new(&e.to_string())
+impl From<IoError> for Metadata {
+    fn from(e: IoError) -> Metadata {
+        Metadata::new(&e.to_string())
     }
 }
 
-impl From<XMLError> for MetadataError {
-    fn from(e: XMLError) -> MetadataError {
-        MetadataError::new(&e.to_string())
+impl From<XMLError> for Metadata {
+    fn from(e: XMLError) -> Metadata {
+        Metadata::new(&e.to_string())
     }
 }
